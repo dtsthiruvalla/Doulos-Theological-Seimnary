d29965c56fd07c9e39d69ce3566acd686670915c
dtsthiruvalla <dts.thiruvalla@gmail.com>
2025-07-18 01:56:32 +0530

    Add basic PHP test script and index HTML for DTS API server
    - Created a new PHP file (test-basic.php) to verify PHP functionality on the server.
    - Added an HTML index file (godaddy-index.html) to serve as the main page for the DTS API server, including links to various API test endpoints.

---
+++ api/test-basic.php
@@ -0,0 +1,6 @@
+<?php
+echo "SUCCESS: PHP is working on GoDaddy!<br>";
+echo "Current time: " . date('Y-m-d H:i:s') . "<br>";
+echo "Server: " . $_SERVER['SERVER_NAME'] . "<br>";
+echo "PHP Version: " . phpversion();
+?>
