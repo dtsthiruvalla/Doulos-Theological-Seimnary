187a2c0342ce2f71e7a573637aa6969f089f81df
dtsthiruvalla <dts.thiruvalla@gmail.com>
2025-07-18 12:56:00 +0530

    feat: Remove deprecated API files to clean up project structure

--- api/test-basic.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "SUCCESS: PHP is working on GoDaddy!<br>";
-echo "Current time: " . date('Y-m-d H:i:s') . "<br>";
-echo "Server: " . $_SERVER['SERVER_NAME'] . "<br>";
-echo "PHP Version: " . phpversion();
